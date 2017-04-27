d85087d3e22d8a3ac789c21f96fd82cda09cab94
Kenny Pflug <kenny.pflug@live.de>
2017-04-27 17:24:04 +0200

    parameterized root certificate name

--- TLS/CreateDerivedCertificate.bat
+++ TLS/CreateDerivedCertificate.bat
@@ -1,3 +1,3 @@
-makecert.exe -iv feO2xRoot.pvk -ic feO2xRoot.cer -n "CN=%1" -pe -sv %1.pvk -a sha512 -len 2048 -sky exchange %1.cer -eku 1.3.6.1.5.5.7.3.1
+makecert.exe -iv %2.pvk -ic %2.cer -n "CN=%1" -pe -sv %1.pvk -a sha512 -len 2048 -sky exchange %1.cer -eku 1.3.6.1.5.5.7.3.1
 
 pvk2pfx.exe -pvk %1.pvk -spc %1.cer -pfx %1.pfx
--- TLS/CreateRootCertificate.bat
+++ TLS/CreateRootCertificate.bat
@@ -1,3 +1,3 @@
-makecert.exe -r -n "CN=feO2xRoot" -pe -sv feO2xRoot.pvk -a sha512 -len 2048 -cy authority feO2xRoot.cer
+makecert.exe -r -n "CN=%1" -pe -sv %1.pvk -a sha512 -len 2048 -cy authority %1.cer
 
-pvk2pfx.exe -pvk feO2xRoot.pvk -spc feO2xRoot.cer -pfx feO2xRoot.pfx
+pvk2pfx.exe -pvk %1.pvk -spc %1.cer -pfx %1.pfx
