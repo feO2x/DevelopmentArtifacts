f106ffcb64995ccf9df6a3c873b0128ec8b938e4
Kenny Pflug <kenny.pflug@live.de>
2021-01-19 18:47:05 +0100

    fixed missing alias in git history.cmd

--- git/git history.cmd
+++ git/git history.cmd
@@ -1,4 +1,4 @@
-git config --global h "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
-git config --global history "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
-git config --global hd "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
-git config --global history-detailed "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
+git config --global alias.h "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
+git config --global alias.history "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
+git config --global alias.hd "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
+git config --global alias.history-detailed "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
