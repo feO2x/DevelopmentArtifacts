70229b5380c29d414f9e78a2da6b8784286fce5e
Kenny Pflug <kenny.pflug@live.de>
2021-01-19 18:37:08 +0100

    added scripts for git history and git svn remove directories

---
+++ git/git history.cmd
@@ -0,0 +1,4 @@
+git config --global h "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
+git config --global history "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
+git config --global hd "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
+git config --global history-detailed "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
