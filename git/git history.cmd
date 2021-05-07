da67994139179e8c09ddffd5b4295a2907b07bdf
Kenny Pflug <kenny.pflug@live.de>
2021-05-07 18:26:13 +0200

    updated git commands so that they are directly executable on linux

--- git/git history.cmd
+++
@@ -1,4 +0,0 @@
-git config --global h "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
-git config --global history "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
-git config --global hd "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
-git config --global history-detailed "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
---
+++ git/git-history.cmd
@@ -0,0 +1,4 @@
+git config --global alias.h "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
+git config --global alias.history "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s  -  %an'"
+git config --global alias.hd "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
+git config --global alias.history-detailed "log --graph --branches --remotes --tags --format='%C(auto)%h %d %s%n%an %ai%n'"
